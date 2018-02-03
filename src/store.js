648c62b28b348a0ed7d8e2c18d3acec63f83f9a1
Carl Vitullo <carl.vitullo@gmail.com>
2018-02-03 18:55:18 -0500

    Add redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,16 +1,22 @@
 import {
   combineReducers,
-  createStore as reduxCreateStore
+  createStore as reduxCreateStore,
+  applyMiddleware,
+  compose
 } from "redux";
+import thunk from "redux-thunk";
 import { rat } from "./rat/reducers";
 // import world from './world/reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const createStore = initialState =>
   reduxCreateStore(
     combineReducers({
       rat
     }),
-    initialState
+    initialState,
+    composeEnhancers(applyMiddleware(thunk))
   );
 
 export { createStore };
